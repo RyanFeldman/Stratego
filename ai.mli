47172233267fa950c7cb826fa2d9678af5d73524
Chris Sciavolino <cds253@cornell.edu>
2016-11-07 11:49:33 -0500

    Added some preliminary vals and types based on our conversations on Sunday

---
+++ ai.mli
@@ -0,0 +1,16 @@
+(* The [AI] module represents the computer opponent to the stratego user.
+ * it takes in boards and chooses a move based on the user's move. *)
+module type AI = sig 
+	
+	(* What would the board type be? Would that be public as opposed
+	 * to abstracted? *)
+
+	(* [score_board] takes in a board and assigns it a score based on 
+	 * how desirable it is for the AI *)
+	val score_board : board -> int
+
+	(* [choose_best_board] takes in a list of boards available to the AI 
+	 * and picks the one with the highest score (relative to the AI) *)
+	val choose_best_board : board list -> board
+
+end
---
+++ board.mli
@@ -0,0 +1,31 @@
+(* A module type [Board] represents a stratego board with a map from tiles 
+ * to piece options. 
+ *)
+module type Board = sig 
+	
+	(* Can we not abstract these types because the rest of the architecture
+	 * depends on them? *)
+
+	(* The type of the board*)
+	type t
+
+	(* Type representing a location on the stratego board*)
+	type position
+
+	(* Type representing a stratego piece on the board *)
+	type piece
+
+	(* Create an instance of a board to be used for a stratego game *)
+	val instantiate_baord : unit -> t
+
+	(* [is_valid_move] takes in a board and two positions and is true 
+	 * iff the piece in the first position can be moved to the second 
+	 * position. *)
+	val is_valid_move : t -> position -> position -> bool
+
+	(* [get_valid_boards] takes in a current board and returns a list 
+	 * of possible boards that are valid from the current board. Used
+	 * only by the AI *)
+	val get_valid_boards : t -> t list
+
+end
