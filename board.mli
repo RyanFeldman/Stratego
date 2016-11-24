c6919d575aafe7854e7a1f6f06df62ac02feed79
Chris Sciavolino <cds253@cornell.edu>
2016-11-23 19:19:49 -0500

    Fixed the spacing vs tabs issue..?

--- board.mli
+++ board.mli
@@ -5,39 +5,39 @@ module type Board = sig
 
 	(* For Reference: 
 	 *     _________________________________________________
-	 *	  |    |    |    |    |    |    |    |    |    |    | 
+	 *    |    |    |    |    |    |    |    |    |    |    | 
 	 *  J | AI | AI | AI | AI | AI | AI | AI | AI | AI | AI |
-     *    |____|____|____|____|____|____|____|____|____|____|
+	 *    |____|____|____|____|____|____|____|____|____|____|
 	 *    |    |    |    |    |    |    |    |    |    |    | 
-     *  I | AI | AI | AI | AI | AI | AI | AI | AI | AI | AI |
-     *    |____|____|____|____|____|____|____|____|____|____|
+	 *  I | AI | AI | AI | AI | AI | AI | AI | AI | AI | AI |
+	 *    |____|____|____|____|____|____|____|____|____|____|
 	 *    |    |    |    |    |    |    |    |    |    |    | 
-     *  H | AI | AI | AI | AI | AI | AI | AI | AI | AI | AI |
-     *    |____|____|____|____|____|____|____|____|____|____|
+	 *  H | AI | AI | AI | AI | AI | AI | AI | AI | AI | AI |
+	 *    |____|____|____|____|____|____|____|____|____|____|
 	 *    |    |    |    |    |    |    |    |    |    |    | 
-     *  G | AI | AI | AI | AI | AI | AI | AI | AI | AI | AI |
-     *    |____|____|____|____|____|____|____|____|____|____|
+	 *  G | AI | AI | AI | AI | AI | AI | AI | AI | AI | AI |
+	 *    |____|____|____|____|____|____|____|____|____|____|
 	 *    |    |    |    |    |    |    |    |    |    |    | 
-     *  F |    |    |    |    |    |    |    |    |    |    |
-     *    |____|____|____|____|____|____|____|____|____|____|
+	 *  F |    |    |    |    |    |    |    |    |    |    |
+	 *    |____|____|____|____|____|____|____|____|____|____|
 	 *    |    |    |    |    |    |    |    |    |    |    | 
-     *  E |    |    |    |    |    |    |    |    |    |    |
-     *    |____|____|____|____|____|____|____|____|____|____|
+	 *  E |    |    |    |    |    |    |    |    |    |    |
+	 *    |____|____|____|____|____|____|____|____|____|____|
 	 *    |    |    |    |    |    |    |    |    |    |    | 
-     *  D | US | US | US | US | US | US | US | US | US | US |
-     *    |____|____|____|____|____|____|____|____|____|____|
-     *    |    |    |    |    |    |    |    |    |    |    | 
-     *  C | US | US | US | US | US | US | US | US | US | US |
-     *    |____|____|____|____|____|____|____|____|____|____|
-     *    |    |    |    |    |    |    |    |    |    |    | 
-     *  B | US | US | US | US | US | US | US | US | US | US |
-     *    |____|____|____|____|____|____|____|____|____|____|
-     *    |    |    |    |    |    |    |    |    |    |    | 
-     *  A | US | US | US | US | US | US | US | US | US | US |
-     *    |____|____|____|____|____|____|____|____|____|____|
-     *      1     2    3    4    5    6    7    8    9   10
-     * 
-     * This is the board upon instantiation.
+	 *  D | US | US | US | US | US | US | US | US | US | US |
+	 *    |____|____|____|____|____|____|____|____|____|____|
+	 *    |    |    |    |    |    |    |    |    |    |    | 
+	 *  C | US | US | US | US | US | US | US | US | US | US |
+	 *    |____|____|____|____|____|____|____|____|____|____|
+	 *    |    |    |    |    |    |    |    |    |    |    | 
+	 *  B | US | US | US | US | US | US | US | US | US | US |
+	 *    |____|____|____|____|____|____|____|____|____|____|
+	 *    |    |    |    |    |    |    |    |    |    |    | 
+	 *  A | US | US | US | US | US | US | US | US | US | US |
+	 *    |____|____|____|____|____|____|____|____|____|____|
+	 *      1     2    3    4    5    6    7    8    9   10
+	 * 
+	 * This is the board upon instantiation.
 	 *)
 
 	(* The type of the board *)
@@ -64,12 +64,12 @@ module type Board = sig
 	val is_valid_move : t -> position -> position -> bool
 
 	(** 
- 	 * [make_move] takes in a board and a valid movement command from the player, 
- 	 * and returns the resulting board. 
+	 * [make_move] takes in a board and a valid movement command from the player, 
+	 * and returns the resulting board. 
 	 * Requires: 
- 	 * 		- the movement from position1 -> position2 is valid 
- 	 * 		- position1 contains a piece that can execute the movement
- 	 *)
+	 * 		- the movement from position1 -> position2 is valid 
+	 * 		- position1 contains a piece that can execute the movement
+	 *)
 	val make_move : t -> position -> position -> (t * piece list)
 
 end
