e2bacf2903ad76cf8b9c8e8be9d81aa6e82865ca
Dan Laine <ubuntu@csvm>
2016-11-25 11:03:45 -0500

    created .ocamlinit and test.ml. Not sure why test isn't working

---
+++ test.ml
@@ -0,0 +1,23 @@
+open OUnit2
+open Board
+open BoardMap
+open IntTuple
+
+let corner_board =
+  BoardMap.add (0,0) (Some {rank=5; player=false; hasBeenSeen = false})
+    BoardMap.empty
+
+
+let ai_tests = "ai tests" >::: [
+
+    "sample test" >:: (fun _ -> assert_equal 1 1)
+
+    (*"making board 1" >::(fun _ -> assert_equal
+        (Some {rank=5; player=false; hasBeenSeen = false})
+        (BoardMap.find (0,0) corner_board)
+      );
+    *)
+
+    ]
+
+let _ = run_test_tt_main ai_tests
