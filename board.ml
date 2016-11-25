a1da921d6d88f780f0e14feb4e8bc1e24098c8b9
Dan Laine <ubuntu@csvm>
2016-11-25 16:35:01 -0500

    I suck at these

--- board.ml
+++ board.ml
@@ -1,6 +1,6 @@
-module type Tuple = sig 
+module type Tuple = sig
     type t
-    val compare : t -> t -> bool
+    val compare : t -> t -> int
 end
 
 module IntTuple : Tuple = struct
@@ -17,13 +17,13 @@ module type Board = sig
     type t
     type position
     type piece
-    module BoardMap : Map.S
+    module BoardMap : Map.S with type key = (int * int)
     val instantiate_board : unit -> t
     val is_valid_move : t -> position -> position -> (bool * string)
     val make_move : t -> position -> position -> (t * piece list)
 end
 
-module Board = struct
+module GameBoard : Board = struct
 
     type position = int * int
 
@@ -33,48 +33,48 @@ module Board = struct
         rank : int;
         player : bool;
         hasBeenSeen : bool
-    }   
+    }
 
-    module BoardMap = Map.Make(IntTuple)
+    module BoardMap = Map.Make(IntTuple) with type t = int*int
 
 	type t = (piece option) BoardMap.t
 
     let rec get_user_input board = failwith "Unimplemented"
 
-    let rec instantiate_user_board board = function 
-    | [] -> board 
-    | h::t -> 
-        let new_board = get_user_input board in 
-        instantiate_user_board new_board t 
+    let rec instantiate_user_board board = function
+    | [] -> board
+    | h::t ->
+        let new_board = get_user_input board in
+        instantiate_user_board new_board t
 
-    let get_list_all_pieces () = 
-        let p       = {rank=0; player=false; hasBeenSeen=false} in 
-        let col     = {p with rank=8} in 
-        let major   = {p with rank=7} in 
-        let cap     = {p with rank=6} in 
-        let lieut   = {p with rank=5} in 
-        let serg    = {p with rank=4} in 
-        let miner   = {p with rank=3} in 
-        let scout   = {p with rank=2} in 
-        let bomb_list = [p; p; p; p; p; p] in 
-        let marsh_list = [{p with rank=10}] in 
-        let gen_list = [{p with rank=9}] in 
-        let col_list = [col; col] in 
-        let maj_list = [major; major; major] in 
-        let cap_list = [cap; cap; cap; cap] in 
-        let lieut_list = [lieut; lieut; lieut; lieut] in 
-        let serg_list = [serg; serg; serg; serg] in 
-        let mine_list = [miner; miner; miner; miner; miner] in 
-        let sco_list = [scout; scout; scout; scout; scout; scout; scout; scout] in 
-        let spy_list = [{p with rank=1}] in 
-        let flag_lst = [{p with rank=11}] in 
+    let get_list_all_pieces () =
+        let p       = {rank=0; player=false; hasBeenSeen=false} in
+        let col     = {p with rank=8} in
+        let major   = {p with rank=7} in
+        let cap     = {p with rank=6} in
+        let lieut   = {p with rank=5} in
+        let serg    = {p with rank=4} in
+        let miner   = {p with rank=3} in
+        let scout   = {p with rank=2} in
+        let bomb_list = [p; p; p; p; p; p] in
+        let marsh_list = [{p with rank=10}] in
+        let gen_list = [{p with rank=9}] in
+        let col_list = [col; col] in
+        let maj_list = [major; major; major] in
+        let cap_list = [cap; cap; cap; cap] in
+        let lieut_list = [lieut; lieut; lieut; lieut] in
+        let serg_list = [serg; serg; serg; serg] in
+        let mine_list = [miner; miner; miner; miner; miner] in
+        let sco_list = [scout; scout; scout; scout; scout; scout; scout; scout] in
+        let spy_list = [{p with rank=1}] in
+        let flag_lst = [{p with rank=11}] in
         bomb_list @ marsh_list @ gen_list @ col_list @ maj_list @ cap_list @
         lieut_list @ serg_list @ mine_list @ sco_list @ spy_list @ flag_lst
 
 	let instantiate_board () : t =
-		let new_board = BoardMap.empty in 
-        let full_pieces = get_list_all_pieces () in 
-        let user_board = instantiate_user_board new_board full_pieces in 
+		let new_board = BoardMap.empty in
+        let full_pieces = get_list_all_pieces () in
+        let user_board = instantiate_user_board new_board full_pieces in
         user_board
 
 	let is_valid_move (board:t) (pos_one:position) (pos_two:position) : bool * string =
--- board.mli
+++ board.mli
@@ -1,18 +1,17 @@
 
 
-module type Tuple = sig 
+module type Tuple = sig
     type t
-    val compare : t -> t -> bool
+    val compare : t -> t -> int
 end
 
-module IntTuple : Tuple
+module IntTuple : Tuple with type t = (int*int)
 
 
 (* A module type [Board] represents a stratego board with pieces
  * on tiles.
  *)
 module type Board = sig
-
 	(* For Reference:
 	 *     _________________________________________________
 	 *    |    |    |    |    |    |    |    |    |    |    |
@@ -50,7 +49,7 @@ module type Board = sig
 	 * This is the board upon instantiation.
 	 *)
 
-    module BoardMap : Map.S
+    module BoardMap : Map.S with type key = (int*int)
 
 	(* The type of the board *)
 	type t
@@ -72,10 +71,10 @@ module type Board = sig
 	val instantiate_board : unit -> t
 
 	(**
-	 * [is_valid_move] takes in a board and two positions and is true iff the 
-	 * piece in the first position can be moved to the second position legally 
-	 * by the rules of stratego. If position1 does not contain a piece, 
-	 * [is_valid_move] is false. Returns an error string if [is_valid_move] is 
+	 * [is_valid_move] takes in a board and two positions and is true iff the
+	 * piece in the first position can be moved to the second position legally
+	 * by the rules of stratego. If position1 does not contain a piece,
+	 * [is_valid_move] is false. Returns an error string if [is_valid_move] is
      * false with the reason why to display to the user. Empty if true.
 	 *)
 	val is_valid_move : t -> position -> position -> (bool * string)
