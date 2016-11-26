17e140225f932cd3b27c76e1c4a55d94211fbb28
Chris Sciavolino <cds253@cornell.edu>
2016-11-26 13:30:37 -0500

    Removed unnecessary code from top of test.ml. Added some test cases for is_valid_move

--- test.ml
+++ test.ml
@@ -1,29 +1,25 @@
 open OUnit2
-open Board
+open Board.GameBoard
 
-(* piece.player = true -> AI
- * piece.player = false -> User *)
-type piece = {
-    rank : int;
-    player : bool;
-    hasBeenSeen : bool
-}   
-
-module IntTuple = struct
-
-    type t = (int * int)
-
-    let compare (t1:t) (t2:t) =
-        match Pervasives.compare (fst t1) (fst t2) with
-            | 0 -> Pervasives.compare (snd t1) (snd t2)
-            | c -> c
-end
-
-module BoardMap = Map.Make(IntTuple)
+let bomb = {rank=0; player=false; hasBeenSeen=false}
 
 let corner_board =
     BoardMap.add (0,0) (Some {rank=5; player=false; hasBeenSeen = false})
         BoardMap.empty
+
+let flag_top_row = 
+    BoardMap.add (3, 5) (Some {bomb with rank=11}) BoardMap.empty
+
+let cap_first_row = 
+    BoardMap.add (0, 3) (Some {bomb with rank=6}) BoardMap.empty
+let cap_first_row_none = 
+    BoardMap.add (0, 4) (None) cap_first_row
+let cap_first_row_ally = 
+    BoardMap.add (0, 4) (Some {bomb with rank=6}) cap_first_row_none
+let cap_first_row_enemy = 
+    BoardMap.add (0, 4) (Some {bomb with player=true}) cap_first_row_none
+let cap_first_row_far =     
+    BoardMap.add (0, 5) (None) cap_first_row_none
 
 let tests = "ai tests" >::: [
 
@@ -32,6 +28,30 @@ let tests = "ai tests" >::: [
     "making board 1" >:: (fun _ -> assert_equal
         (Some {rank=5; player=false; hasBeenSeen = false})
         (BoardMap.find (0,0) corner_board));
+
+    "is_valid_move_flag" >:: (fun _ -> assert_equal 
+        (false, "That piece can't move there!")
+        (is_valid_move flag_top_row false (3, 5) (3, 6)));
+
+    "is_valid_move_valid" >:: (fun _ -> assert_equal
+        (true, "")
+        (is_valid_move cap_first_row_none false (0, 3) (0, 4)));
+
+    "is_valid_move_ally" >:: (fun _ -> assert_equal
+        (false, "Don't attack your own team!")
+        (is_valid_move cap_first_row_ally false (0, 3) (0, 4)));
+
+    "is_valid_move_enemy" >:: (fun _ -> assert_equal
+        (true, "")
+        (is_valid_move cap_first_row_enemy false (0, 3) (0, 4)));
+
+    "is_valid_move_off_left" >:: (fun _ -> assert_equal
+        (false, "Position outside of board")
+        (is_valid_move cap_first_row_none false (0, 3) (-1, 3)));
+
+    "is_valid_move_too_far" >:: (fun _ -> assert_equal
+        (false, "That piece can't move there!")
+        (is_valid_move cap_first_row_far false (0, 3) (0, 5)));
     
 
     ]
