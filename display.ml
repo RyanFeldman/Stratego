c1b792ac815c8525cbce2ff283e578b35cb509ef
Ryan Feldman <rpf53@cornell.edu>
2016-11-25 23:03:08 -0500

    Fixed some shizzle

--- display.ml
+++ display.ml
@@ -1,6 +1,6 @@
 open Board.GameBoard
 
-type board = Board.GameBoard.t
+type board = (piece option) BoardMap.t
 
 module type Display = sig
 
@@ -20,19 +20,29 @@ module TextDisplay : Display = struct
 
   let print_message s = print_endline s
 
+  let string_of_piece p = match p.rank with
+  |x when x < 10 -> " "^(string_of_int x)
+  |x -> string_of_int x
+
   let print_row b row =
     for col = 0 to 9 do
-      (*let piece = Map.find*)
-      print_string "___|_"
+      let p = BoardMap.find (col, row) b in ();
+      (match p with
+      |None -> print_string "  ";
+      |Some x -> if x.player then print_string "  "
+        else (print_string (string_of_piece x)));
+      print_string " | "
     done;
     print_endline ""
 
   let display_board b =
     print_endline "     _________________________________________________";
-    print_endline "    |    |    |    |    |    |    |    |    |    |    | ";
     for row = 0 to 9 do
-      print_string "    | ";
-      print_row b row
+
+      print_endline "    |    |    |    |    |    |    |    |    |    |    | ";
+      print_string  "    | ";
+      print_row b row;
+      print_endline "    |____|____|____|____|____|____|____|____|____|____|";
     done
 
 end
