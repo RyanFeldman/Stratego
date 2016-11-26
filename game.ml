93ab0d575d1a445cf7717c4b289589b44428918d
Ryan Feldman <rpf53@cornell.edu>
2016-11-25 21:54:29 -0500

    Added game.ml

---
+++ game.ml
@@ -0,0 +1,13 @@
+open Board.GameBoard
+
+module Game = struct
+
+type board = Board.t
+
+(*Just so shit runs, not implemented yet*)
+let setup_game () = Board.instantiate_board ()
+
+(*Just so shit runs, not implemented yet*)
+let play a = a
+
+end
