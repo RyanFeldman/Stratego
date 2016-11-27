f2526752393e4ad73c9e10a2a17592301ec33790
Chris Sciavolino <cds253@cornell.edu>
2016-11-27 00:12:42 -0500

    added some specs to helper functions

--- board.ml
+++ board.ml
@@ -64,48 +64,48 @@ module GameBoard : Board = struct
     let board_iter f board = BoardMap.iter f board
 
     let string_from_piece piece = 
-    match piece.rank with 
-    | 0 -> "Bomb"
-    | 1 -> "Spy"
-    | 2 -> "Scout"
-    | 3 -> "Miner"
-    | 4 -> "Sergeant"
-    | 5 -> "Lieutenant"
-    | 6 -> "Captain"
-    | 7 -> "Major"
-    | 8 -> "Colonel"
-    | 9 -> "General"
-    | 10 -> "Marshal"
-    | 11 -> "Flag"
-    | _ -> failwith "Not a valid piece"
+        match piece.rank with 
+        | 0 -> "Bomb"
+        | 1 -> "Spy"
+        | 2 -> "Scout"
+        | 3 -> "Miner"
+        | 4 -> "Sergeant"
+        | 5 -> "Lieutenant"
+        | 6 -> "Captain"
+        | 7 -> "Major"
+        | 8 -> "Colonel"
+        | 9 -> "General"
+        | 10 -> "Marshal"
+        | 11 -> "Flag"
+        | _ -> failwith "Not a valid piece"
 
     let rec step board b pos dir = 
         match dir with 
         | N -> 
             let (x, y) = pos in 
             if y=9 then [] else
-            (match (BoardMap.find (x, y+1) board) with 
+            (match (search (x, y+1) board) with 
             | None -> (x, y+1)::(step board b (x, y+1) N)
             | Some piece -> 
                 if (piece.player = b) then [] else [(x, y+1)])
         | E -> 
             let (x, y) = pos in 
             if x=9 then [] else
-            (match (BoardMap.find (x+1, y) board) with 
+            (match (search (x+1, y) board) with 
             | None -> (x+1, y)::(step board b (x+1, y) E)
             | Some piece -> 
                 if (piece.player = b) then [] else [(x+1, y)])
         | S -> 
             let (x, y) = pos in 
             if y=0 then [] else
-            (match (BoardMap.find (x, y-1) board) with 
+            (match (search (x, y-1) board) with 
             | None -> (x, y-1)::(step board b (x, y-1) S)
             | Some piece -> 
                 if (piece.player = b) then [] else [(x, y-1)])
         | W -> 
             let (x, y) = pos in 
             if x=0 then [] else
-            (match (BoardMap.find (x-1, y) board) with 
+            (match (search (x-1, y) board) with 
             | None -> (x-1, y)::(step board b (x-1, y) W)
             | Some piece -> 
                 if (piece.player = b) then [] else [(x-1, y)])
@@ -135,39 +135,55 @@ module GameBoard : Board = struct
             if (snd pos) < 0 || (fst pos) > 9 then false
         else true
 
+    (**
+     * [check_pos_one board b pos] is a tuple t. Fst t is true iff a piece at 
+     * [pos] has field player that is equal to b. False otherwise. Snd t is 
+     * a string containing the reason why the move is not valid. "" if valid.
+     *)
     let check_pos_one board b pos = 
-         match (BoardMap.find pos board) with 
+         match (search pos board) with 
             | None -> (false, "There's nothing at (" ^ (string_of_int (fst pos))
                                 ^ ", " ^ (string_of_int (snd pos)) ^ ")!")
             | Some x -> 
-                if (not (x.player = b)) then (false, "That's not your piece!")
-                else (true, "")
+                if x.player = b then (true, "")
+                else (false, "That's not your piece!")
 
+    (* See board.mli *)
     let is_valid_move (board:t) (b:bool) (pos_one:position) (pos_two:position) 
             : bool * string =
         let within_board = (in_board pos_one) && (in_board pos_two) in 
         if (within_board = false) then (false, "Position outside of board") else
         let valid_pos_one = check_pos_one board b pos_one in 
         if (fst valid_pos_one) = false then valid_pos_one else 
-        let pos_one_p = match (BoardMap.find pos_one board) with 
+        let pos_one_p = match (search pos_one board) with 
                         | None -> failwith "Nothing at pos_one for some reason"
                         | Some x -> x in 
         let possible_moves_list = get_possible_moves board b pos_one_p pos_one in
         if (List.mem pos_two possible_moves_list) = false 
             then (false, "That piece can't move there!") 
         else 
-            match (BoardMap.find pos_two board) with 
+            match (search pos_two board) with 
             | None -> (true, "")
             | Some x -> 
                 if (x.player = b) then (false, "Don't attack your own team!")
                 else
                     (true, "")
 
+    (**
+     * [remove_optional piece] is p in [piece], which is (Some p).
+     * Raises: 
+     *  - Failure if [piece] is None
+     *)
     let remove_optional piece = 
         match piece with 
         | None -> failwith "None"
         | Some p -> p
 
+    (**
+     * [execute_conflict board p_two pos_one pos_two] is a tuple containing 
+     * the new board after executing the movement of the piece from [pos_one] 
+     * to [pos_two] on [board] and the list of pieces captured in the conflict.
+     *)
     let execute_conflict board p_two pos_one pos_two = 
         let p_one = remove_optional (search pos_one board) in 
         match (p_one.rank - p_two.rank) with 
@@ -188,10 +204,17 @@ module GameBoard : Board = struct
             (new_board, [p_two])
         | _ -> failwith "invalid math"
 
+    (**
+     * [string_from_tuple t] is the string form of the tuple [t]. i.e. "(x, y)"
+     *)
     let string_from_tuple t = 
         let (x, y) = t in 
         "("^(string_of_int x)^", "^(string_of_int y)^")"
 
+    (**
+     * [get_msg pos_one pos_two tup] is the message to be posted to the user 
+     * depending on how (or if) a conflict occured. 
+     *)
     let get_msg pos_one pos_two tup = 
         match (snd tup) with  
         | [] -> ""
@@ -213,6 +236,7 @@ module GameBoard : Board = struct
             ^" taken from the board."
         | _ -> failwith "Invalid captured pieces list given"
 
+    (* See board.mli file *)
     let make_move (board:t) (pos_one:position) (pos_two:position) 
             : t * piece list =
         let changed_tuple = 
@@ -231,10 +255,5 @@ module GameBoard : Board = struct
                                 ^"conflicts!") in 
         let _ = print_endline msg in
         changed_tuple
-
-
-
-
-
 
 end
