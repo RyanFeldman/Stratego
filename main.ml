d2a30f1bfe3f463f4d656a824e7a692369a6e888
Ryan Feldman <rpf53@cornell.edu>
2016-11-29 00:44:18 -0500

    Minor tweak, got rid of function

--- main.ml
+++ main.ml
@@ -1,10 +1,11 @@
 open Game
 
 (* See main.mli *)
-let play_game () =
-  print_string "\n\nWelcome to Stratego.\n
-    Please make your terminal full screen for the best experience.\n";
-  print_endline "Type RULES to learn how to play or PLAY to start.";
+let () =
+  print_string "\n\n  Welcome to Stratego.\n
+  Please make your terminal full screen for the best experience.\n\n";
+  print_endline "  Type RULES to learn how to play or PLAY to start.\n";
+  print_string ">";
   let user_input = read_line () in
   let trimmed = user_input |> String.trim |> String.lowercase_ascii in
   let () = if (trimmed = "rules") then (
@@ -12,5 +13,5 @@ let play_game () =
     print_endline "Input anything to continue...";
     let _ = read_line () in ()) else () in
   let initial_board = setup_game () in
-  let dumb = (fun x -> ()) in
-  dumb (play initial_board)
+  let _ = play initial_board in
+  ()
